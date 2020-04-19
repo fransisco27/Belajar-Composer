8e75525629bfdf7fb7eea0a1ded1ca9ecd4e726c
Fransisco Allagan <63401643+fransisco27@users.noreply.github.com>
2020-04-19 18:20:51 +0700

    report.php
    berisi source code report

---
+++ report.php
@@ -0,0 +1,14 @@
+<?php
+require 'vendor/autoload.php';
+
+use Dompdf\Dompdf;
+
+$dompdf = new Dompdf();
+$dompdf->loadHtml('Menggunakan Library DOMpdf untuk Membuat Report PDF dengan DOMPDF');
+
+$dompdf->setPaper('A4,', 'landscape');
+
+$dompdf->render();
+
+$dompdf->stream('hasil_report.pdf');
+?>
